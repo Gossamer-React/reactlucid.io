b9465301b147a8576342594fb62740beebf9b8c8
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-12-06 16:47:02 -0500

    deleting components that werent being used

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 import '../app.css';
-import logoWhite from'./../../../public/assets/logo-white.png'
+import logoWhite from './../../../public/assets/logo-white.png'
 // import NavBar from './NavBar.jsx'
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+
     }
   }
-  
+
   componentDidMount() {
-    
+
   }
-  
-  
+
+
   render() {
 
-    return(
-      <div id='splash'>
-        <img id='logo' src={logoWhite} />
-        <h1>a React-GraphQL devtool</h1>
-        <div>
-          <h2>Please give us stars!!!</h2>
-          <button>Install</button>
+    return (
+      <div>
+        <div id='splash'>
+          <img id='logo' src={logoWhite} />
+          <h1>a React-GraphQL devtool</h1>
+          <div>
+            <h2>Please give us stars!!!</h2>
+            <button>Install</button>
+          </div>
         </div>
       </div>
     )
