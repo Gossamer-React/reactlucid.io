bb2944209edb440257a2e2598a68df5126bf92f1
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-12-10 18:54:12 -0500

    Update About.jsx

--- src/client/components/About.jsx
+++ src/client/components/About.jsx
@@ -8,7 +8,7 @@ import yongImg from './../../../public/assets/team/yong.jpg';
 const About = () => {
   return (
     <div>
-      <h2>About Our Team</h2>
+      <h2>Meet Our Developers</h2>
       <div id='team'>
         <div className ='person'>
           <img className='pro-img' src={eternaImg} />
