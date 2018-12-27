bd12239ee66aae98439f51041a5bb439b91a9755
eternalee <one.eternity@gmail.com>
2018-12-27 11:33:55 -0800

    compression and footer changes

--- src/client/components/About.jsx
+++ src/client/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../about.css';
 import eternaImg from './../../../public/assets/team/eterna.jpg';
-import nianImg from './../../../public/assets/team/nian.png';
+import nianImg from './../../../public/assets/team/nian.jpeg';
 import neyserImg from './../../../public/assets/team/neyser.jpg';
 import yongImg from './../../../public/assets/team/yong.jpg';
 
@@ -13,22 +13,22 @@ const About = () => {
         <div className ='person'>
           <img className='pro-img' src={eternaImg} />
           <h4 className='name'>Eterna</h4>
-          <p className='single-p'><a href = 'https://twitter.com/eterna_t'>@eterna_t</a></p>
+          <p className='single-p'><a href = 'https://github.com/eternalee' target='_blank'>@eternalee</a></p>
         </div>
         <div className ='person'>
           <img className='pro-img' src={neyserImg} />
           <h4 className='name'>Neyser</h4>
-          <p className='single-p'><a href = 'https://twitter.com/eterna_t'>@eterna_t</a></p>
+          <p className='single-p'><a href = 'https://github.com/neyser95' target='_blank'>@neyser95</a></p>
         </div>
         <div className ='person'>
           <img className='pro-img' src={nianImg} />
           <h4 className='name'>Nian</h4>
-          <p className='single-p'><a href = 'https://twitter.com/niantendo'>@niantendo</a></p>
+          <p className='single-p'><a href = 'https://github.com/nianliu18' target='_blank'>@nianliu18</a></p>
         </div>
         <div className ='person'>
           <img className='pro-img' src={yongImg} />
           <h4 className='name'>Yong-Nicholas</h4>
-          <p className='single-p'><a href = 'https://twitter.com/yongnicholaskim'>@yongnicholaskim</a></p>
+          <p className='single-p'><a href = 'https://github.com/yongnicholaskim' target='_blank'>@yongnicholaskim</a></p>
         </div>
       </div>
     </div>
